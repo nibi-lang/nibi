ceb0a3711e2f5883acd8311955f29d0000062f7e
bosley <bosley117@gmail.com>
2023-07-22 14:36:49 -0400

    meta

---
+++ nibi/modules/meta/lib.cpp
@@ -0,0 +1,9 @@
+#include "lib.hpp"
+
+#include <iostream>
+#include <libnibi/macros.hpp>
+
+nibi::cell_ptr meta_cell(nibi::cell_processor_if &ci, nibi::cell_list_t &list,
+                         nibi::env_c &env) {
+  return nibi::allocate_cell((int64_t)sizeof(nibi::cell_c));
+}
