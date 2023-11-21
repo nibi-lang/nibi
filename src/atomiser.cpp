8b21401bb64436b3cb5ee9e8bf07d8c10bf673ea
bosley <bosley117@gmail.com>
2023-11-20 20:15:05 -0500

    lmaoo

--- src/atomiser.cpp
+++ src/atomiser.cpp
@@ -1,6 +1,58 @@
 #include "atomiser.hpp"
+
 #include <map>
 #include <regex>
+#include <stack>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+class atomiser_c {
+public:
+  atomiser_c() = delete;
+  atomiser_c(
+    const std::string& origin,
+    list_receiver_if &receiver);
+
+  void submit(const char* data, size_t line=0);
+  void submit(std::string &data, size_t line=0);
+  bool finish();
+
+private:
+  std::string _origin;
+  list_receiver_if &_receiver;
+
+  struct {
+    size_t line{0};
+    size_t col{0};
+  } _trace;
+
+  void emit_error(
+    const std::string& message,
+    const file_position_s& pos) {
+    _receiver.on_error(
+      file_error_s{ _origin, message, pos});
+  }
+
+  void emit_error(
+    const std::string& message) {
+    emit_error(
+      message, 
+      file_position_s{_trace.line, _trace.col});
+  }
+
+  void parse(std::string &string_data);
+  void reset();
+
+  bool collect_comments(std::string& data, atom_list_t* list);
+  bool collect_number(std::string& data, atom_list_t* list);
+  bool collect_string(std::string& data, atom_list_t* list);
+  bool collect_identifier(std::string& data, atom_list_t* list);
+  bool collect_symbol(std::string& data, atom_list_t* list);
+
+  std::stack<atom_list_t> _active_lists;
+  std::set<uint8_t> _termChars;
+};
 
 namespace {
 
@@ -299,8 +351,64 @@ bool atomiser_c::collect_symbol(std::string& data, atom_list_t* list) {
   return true;
 }
 
+class receiver_c : public list_receiver_if {
+public:
+  void on_list(atom_list_t list) override {
+    // print_atom_list(list);
+
+    // TODO: Check here to see if the list is an import.
+    //       If it is, we can import it here. 
+
+    lists.push_back(
+      std::move(list));
+  }
+
+  void on_error(file_error_s err) override {
+    fmt::print("Got error! : {}\n", err.to_string());
+    okay = false;
+  }
+
+  bool okay{true};
+  std::vector<atom_list_t> lists;
+};
+
 void atomiser_c::reset() {
   _trace = {0,0};
   _active_lists = {};
 }
 
+std::optional<parse_group_s> atomise_file(const std::string& file) {
+
+  {
+    std::filesystem::path p(file);
+    if (!std::filesystem::is_regular_file(p)) {
+      fmt::print("Target '{}' is not a file\n", file);
+      return std::nullopt;
+    }
+  }
+
+  std::ifstream in(file);
+  if (!in.is_open()) {
+    fmt::print("Unable to open target '{}'\n", file);
+    return std::nullopt;
+  }
+
+  receiver_c recv;
+  atomiser_c atomiser(file, recv);
+
+  std::string line;
+  std::size_t line_number{1};
+  while (recv.okay && std::getline(in, line)) {
+    atomiser.submit(line, line_number++);
+  }
+
+  in.close();
+
+  return { parse_group_s { file, std::move(recv.lists) }};
+}
+
+int repl() {
+
+  fmt::print("REPL NOT YET COMPLETED");
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,20 @@
 #include <fmt/format.h>
 
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cctype>
-
 #include "atomiser.hpp"
-
-class receiver_c : public list_receiver_if {
-public:
-  void on_list(atom_list_t list) override {
-    fmt::print("Got list :: ");
-    print_atom_list(list);
-    fmt::print("\n\n");
-
-  }
-
-  void on_error(file_error_s err) override {
-    fmt::print("Got error! : {}\n", err.to_string());
-  }
-};
 
 int main(int argc, char** argv) {
 
-  std::vector<std::string> input = {
-   // "(+ 1",
-   // "2.3) (- 10 2)(",
-   // "* 4 (+ 10 11))"
- //  "(+ 1 (* 10 2) (/ x ",
- //    "3.33))"
- 
-//    "(+ a (- b (* d e))",
-//      ")(println yay)"
+  std::optional<parse_group_s>file_group = atomise_file("test.nib");
 
-  //  R"((println "I has a \"NESTED \"string\"\"\t\"thing\" astring"))"
-
-
-
-
-    "(+ ; Some commentary",
-    "1 2 3) ; Ending commenttary",
-    "; One more for fun"
-  };
-  
-  receiver_c recv;
-
-  atomiser_c atomiser("test", recv);
-  
-  for(size_t i = 0; i < input.size(); i++) {
-    atomiser.submit(input[i], i+1);
+  if (!file_group.has_value()) {
+    fmt::print("No parse group returned\n");
+    return 1;
   }
+
+  fmt::print(
+    "File: '{}' returned '{}' lists\n",
+    (*file_group).origin,
+    (*file_group).lists.size());
 
   return 0;
 }
